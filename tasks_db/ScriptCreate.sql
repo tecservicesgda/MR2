0687b685ba11f916e785678e4f12253aaa5575fb
tecservicesgda <112429879+tecservicesgda@users.noreply.github.com>
2023-06-15 22:47:39 -0600

    Create ScriptCreate.sql

---
+++ tasks_db/ScriptCreate.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `dueDate` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=224 DEFAULT CHARSET=utf8;
